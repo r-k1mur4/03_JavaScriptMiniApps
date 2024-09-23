04249d8f68d929f415a6f8ebd8bac315b0b0fe64
Ryuki Kimura <ryuki.kimura@gaxi.co.jp>
2024-09-23 14:02:12 +0900

    🎨 タイトル追加と背景色修正

--- MySlideshow/main.js
+++ MySlideshow/main.js
@@ -1,22 +1,24 @@
 "use strict";
 
+
+/* ・サムネイルクリック時にメイン画像を切り替える
+  └ DOMの取得＞サムネイル・メイン画像
+  └ サムネイルクリック時のイベントリスナー
+・矢印ボタンクリック時にメイン画像を切り替える
+  └ 今何番目かの変数を設定
+  └ 矢印クリック時にメイン画像のsrc属性を次の画像へ切り替え
+  └ サムネイルのクラスリストもactiveクラスを付け替える */
+
+
 {
   // DOMの取得
   const thumbnails = document.querySelectorAll(".thumbnail");
   const mainImage = document.getElementById("main-image");
   const next = document.getElementById("next");
   const prev = document.getElementById("prev");
-
   let indexActive = 0;
 
-  // サムネイルクリック時のイベントリスナー
-  thumbnails[1].addEventListener("click", () => {
-    indexActive = 1;
-    mainImage.src = thumbnails[1].src;
-    thumbnails[0].classList.remove("active");
-    thumbnails[2].classList.remove("active");
-    thumbnails[1].classList.add("active");
-  });
+
 
   // サムネイルクリック時のイベントリスナー
   thumbnails[0].addEventListener("click", () => {
@@ -25,6 +27,15 @@
     thumbnails[0].classList.add("active");
     thumbnails[2].classList.remove("active");
     thumbnails[1].classList.remove("active");
+  });
+
+  // サムネイルクリック時のイベントリスナー
+  thumbnails[1].addEventListener("click", () => {
+    indexActive = 1;
+    mainImage.src = thumbnails[1].src;
+    thumbnails[0].classList.remove("active");
+    thumbnails[2].classList.remove("active");
+    thumbnails[1].classList.add("active");
   });
 
   // サムネイルクリック時のイベントリスナー
