e5a711e6a7d28beb852b0d55289312a6e1ef91f8
Ryuki Kimura <ryuki.kimura@gaxi.co.jp>
2024-09-23 17:30:08 +0900

    ✨ テキストが空欄の場合はボタンをクリック不可。入力されたらクリック復活。

--- MyMemoApp/main.js
+++ MyMemoApp/main.js
@@ -1,21 +1,53 @@
 "use strict";
 
 {
+  // DOMの取得
   const text = document.getElementById("text");
   const save = document.getElementById("save");
+  const clear = document.getElementById("clear");
   const msg = document.getElementById("msg");
 
-  if (localStorage.getItem('memo') === null) {
-    text.value = '';
-  } else {
-    text.value = localStorage.getItem('memo');
+
+  // 初期状態をチェック
+  checkTextarea();
+  // テキストエリアの内容が変わるたびにチェック
+  text.addEventListener("input", checkTextarea);
+
+  function checkTextarea() {
+    // テキストボックスの中身が空のとき
+    if (text.value.trim() === "") {
+      // ボタンをクリック不可に
+      save.classList.add("unclickable");
+      clear.classList.add("unclickable");
+    } else {
+      // ボタンをクリック可能に
+      save.classList.remove("unclickable");
+      clear.classList.remove("unclickable");
+    }
   }
 
+  if (localStorage.getItem("memo") === null) {
+    text.value = "";
+  } else {
+    text.value = localStorage.getItem("memo");
+  }
+
+  // 保存機能
   save.addEventListener("click", () => {
     msg.classList.add("appear");
     setTimeout(() => {
       msg.classList.remove("appear");
     }, 1000);
     localStorage.setItem("memo", text.value);
+  });
+
+  // 削除機能
+  clear.addEventListener("click", () => {
+    if (confirm("本当に削除しますか？") === true) {
+      // テキストエリアの初期化
+      text.value = "";
+      // ローカルストレージ＞memoの削除
+      localStorage.removeItem("memo");
+    }
   });
 }
